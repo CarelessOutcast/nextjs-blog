6b50162a8aa8d803523b30966fef7063494b890f
CarelessOutcast <tapiacarlos32@gmail.com>
2023-07-06 18:57:58 -0500

    Fix image

--- components/layout.js
+++ components/layout.js
@@ -22,10 +22,10 @@ export default function Layout({children, home}){
       </Head>
     <header className={styles.header}>
     {home ? (<>
-      <Image priority src="../public/images/profile.jpg" className={utilStyles.borderCircle} height={144} width={144} alt="" />
+      <Image priority src="./images/profile.jpg" className={utilStyles.borderCircle} height={144} width={144} alt="" />
       <h1 className={utilStyles.heading2Xl}>{name}</h1>
       </>) : (<>
-        <Link href="/"> <Image priority src="../public/images/profile.jpg" className={utilStyles.borderCircle} height={108} width={108} alt="" /> </Link>
+        <Link href="/"> <Image priority src="./images/profile.jpg" className={utilStyles.borderCircle} height={108} width={108} alt="" /> </Link>
         <h2>
         <Link href="/" className="utilStyles.colorInherit"> {name}
         </Link>
